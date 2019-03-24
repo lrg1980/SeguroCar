f85284e7eb0974c603572d589efb89d2fd134e22
lrg1980 <admin@binweb.net>
2019-03-24 09:26:09 -0300

    Fin->transicion en precios, creación modulo precio

--- src/componentes/App.js
+++ src/componentes/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import Formulario from './Formulario';
+import Resumen from './Resumen';
+import Resultado from './Resultado';
 import {obtenerDiferenciaAnio, calcularMarca, obtenerPlan} from '../helper';
 
 class App extends Component {
@@ -60,7 +62,16 @@ class App extends Component {
         <Header titulo = 'Cotizador de Seguro de auto' />
 
         <div className="contenedor-formulario">
-          <Formulario cotizarSeguro={this.cotizarSeguro}/>
+          
+          <Formulario 
+            cotizarSeguro={this.cotizarSeguro}/>
+
+          <Resumen 
+            datos = {this.state.datos}
+            resultado = {this.state.resultado}/>
+
+          <Resultado 
+            resultado={this.state.resultado} />
         </div>
       </div>
     );
